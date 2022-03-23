d3fcd8b22a0e354baf9764c7a5eda83ff544d97c
никита романко <nikita.romanko.2004@gmail.com>
2022-03-23 19:26:58 +0300

    test

--- lab4/Header1.h
+++ lab4/Header1.h
@@ -2,13 +2,14 @@
 #include <math.h>
 using namespace std;
 
-double calculation2(double sh, double rnd, double xb, double y, double a, double x0, double b) {
+double calculation2(double sh, double rnd, double xa, double y, double a, double x0, double b) {
     double x1 = x0 + sh * rnd;
-    double xb = x1;
-    double y1 = cos(xb) + (1 / b) * cos(a * xb + 1) + (1 / pow(b, 2)) * cos(pow(a, 2) * xb + 2) + (1 / pow(b, 3)) * cos(pow(a, 3) * xb + 3) + (1 / pow(b, 4)) * cos(pow(a, 4) * xb + 4);
+    xa = x1;
+    double y1 = cos(xa) + (1 / b) * cos(a * xa + 1) + (1 / pow(b, 2)) * cos(pow(a, 2) * xa + 2) + (1 / pow(b, 3)) * cos(pow(a, 3) * xa + 3) + (1 / pow(b, 4)) * cos(pow(a, 4) * xa + 4);
     if (y1 < y) {
         x0 = x1;
         y = y1;
     }
     sh = 0.75 * sh;
-}
+    return y1;
+};
--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -9,8 +9,10 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "ru");
-    double y, xa = 0, xb, x0, a = 7, b, sh, y1, x1, rnd, i = 1;
-    srand(time(NULL));
+    double y, xa = 0, x0, a = 7, b, sh, y1 = 1, rnd, i = 1, number1, number2 = 0, result;
+    int row, column;
+    srand((unsigned)time(NULL));
+    double numbers[1000][3];
     rnd = (-1.0 + static_cast <float> (rand())) / (static_cast <float> (RAND_MAX / (2.0)));
     cout << "Рандомное число: " << rnd << endl;
     cout << "Введите число b(от 2 до 10): ";
@@ -19,15 +21,46 @@ int main()
         cout << "Написанно ведь: от 2 до 10! Давай еще раз: ";
         cin >> b;
     }
-    cout << "Введите начальное значение: ";
-    cin >> x0;
-    xa = x0;
     cout << "Введите шаг: ";
     cin >> sh;
-    do {
-        double y = calculation1(xa, a, b);
-        double y = calculation2(sh, rnd, xb, y, a, x0, b);
-        cout << fixed << "Итерация номер " << i << "\t" << "Значение y: " << y1 << "\t" << "Значение x: " << x0 << "\t" << "Знаечние шага: " << sh << "\t" << endl;
-        i++;
-    } while (sh > 0.0001);
+    for (row = 0; row < 1000; row++) {
+        for (column = 0; column < 3; column++) {
+            double x0 = (-10.0 + static_cast <float> (rand())) / (static_cast <float> (RAND_MAX / (30.0)));
+            cout << "Начальные значения: " << x0 << endl;
+            xa = x0;
+            y = calculation1(xa, a, b);
+            numbers[row][column] = y;
+            do {
+                //заменить в отдельный файл
+                double x1 = x0 + sh * rnd;
+                xa = x1;
+                double y1 = cos(xa) + (1 / b) * cos(a * xa + 1) + (1 / pow(b, 2)) * cos(pow(a, 2) * xa + 2) + (1 / pow(b, 3)) * cos(pow(a, 3) * xa + 3) + (1 / pow(b, 4)) * cos(pow(a, 4) * xa + 4);
+                if (y1 < y) {
+                    x0 = x1;
+                    y = y1;
+                }
+                sh = 0.75 * sh;
+                //заменить в отдельный файл
+                numbers[row][column] = y1;
+
+            } while (sh > 0.0001);
+        }
+    }
+    cout << "Массив: " << endl;
+    cout << "---------------------------------------------" << endl;
+    for (row = 0; row < 1000; row++) {
+        for (column = 0; column < 3; column++) {
+            cout << numbers[row][column] << " ";
+        }
+        cout << endl;
+    }
+    cout << "---------------------------------------------" << endl;
+    for (row = 0; row < 1000; row++) {
+        for (column = 0; column < 3; column++) {
+            number1 = numbers[row][column];
+            number2 = number2 + number1;
+        }
+    }
+    result = number2 / (3000);
+    cout << "Результат: " << result << endl;
 }
